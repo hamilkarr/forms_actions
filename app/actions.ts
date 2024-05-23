eb0cfd4c4ae5ed8eed88e990de081e1a25475260
hamilkarr <12uclock@gmail.com>
2024-05-24 08:11:18 +0900

    zod 설치

--- app/actions.ts
+++ app/actions.ts
@@ -1,7 +1,20 @@
 'use server';
+import { z } from 'zod';
+
+const formSchema = z.object({
+    email: z.string().email(),
+    username: z.string().min(5),
+    password: z.string().min(10),
+});
 
 export async function handleForm(prevState: any, formData: FormData) {
     await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    const data = {
+        email: formData.get('email'),
+        username: formData.get('username'),
+        password: formData.get('password'),
+    };
 
     if (formData.get('password') === '12345') {
         return {
